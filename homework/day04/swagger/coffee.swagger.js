4df63ac8596dcfbdb0ad6b6fa7ef3391b2bc76b2
kimbo0301 <contempt0301@gmail.com>
2022-04-06 09:33:37 +0900

    회원,커피 목록 조회 & swagger

---
+++ homework/day04/swagger/coffee.swagger.js
@@ -0,0 +1,11 @@
+/**
+ * @swagger
+ * /starbuks:
+ *      get:
+ *          summary: 커피 메뉴 리스트 가져오기
+ *          tags: [Starbuks]
+ *          parameters:
+ *          responses:
+ *              200:
+ *               description: 성공
+ */
