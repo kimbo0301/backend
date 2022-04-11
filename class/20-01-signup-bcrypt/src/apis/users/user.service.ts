4ef19eed610ee99804bbf82b8ef1572bd9eb65a0
kimbo0301 <contempt0301@gmail.com>
2022-04-11 17:34:12 +0900

    signup-bcrypt

---
+++ class/20-01-signup-bcrypt/src/apis/users/user.service.ts
@@ -0,0 +1,24 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async create({ email, hashedPassword, name, age }) {
+    const user = await this.userRepository.findOne({ email });
+    // 중복 이메일인지 확인
+    if (user) throw new ConflictException('이미 등록된 이메일 입니다.');
+
+    return await this.userRepository.save({
+      email,
+      password: hashedPassword,
+      name,
+      age,
+    });
+  }
+}
