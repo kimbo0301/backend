71f06b676a21849444a6e938b27c75b8beb23216
kimbo0301 <contempt0301@gmail.com>
2022-04-19 15:39:30 +0900

    ACID_transaction

---
+++ class/25-01-point-transaction-ACID-transaction/backend/src/apis/pointTransaction/pointTransaction.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, Connection } from 'typeorm';
+import { User } from '../users/entities/user.entity';
+import {
+  PointTransaction,
+  POINT_TRANSACTION_STATUS_ENUM,
+} from './entities/pointTransaction.entity';
+
+@Injectable({})
+export class PointTransactionService {
+  constructor(
+    @InjectRepository(PointTransaction)
+    private readonly pointTransactionRepository: Repository<PointTransaction>,
+
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    private readonly connection: Connection,
+  ) {}
+  async create({ impUid, amount, currentUser }) {
+    const queryRunner = await this.connection.createQueryRunner();
+    await queryRunner.connect();
+
+    // transaction 시작
+    await queryRunner.startTransaction(); //Transaction 시작 쿼리문들은 다 queryRunner에 담으면
+    // 롤백을 통해 실수를 막을 수 있음
+
+    try {
+      // 1. pointTransaction 테블에 거래기록 1줄 생성
+      const pointTransaction = await this.pointTransactionRepository.create({
+        impUid: impUid,
+        amount: amount,
+        user: currentUser,
+        status: POINT_TRANSACTION_STATUS_ENUM.PAYMENT,
+      });
+
+      await queryRunner.manager.save(pointTransaction);
+
+      // 2. user의 돈 찾아오기
+      const user = await this.userRepository.findOne({ id: currentUser.id });
+
+      // 3. 유저의 돈 업데이트
+      // await this.userRepository.update(
+      //   { id: user.id }, // 조건에 해당하는 데이터의
+      //   { point: user.point + amount }, // 업데이트 하고 싶은 내용
+      // );
+      const updatedUser = this.userRepository.create({
+        ...user,
+        point: user.point + amount,
+      });
+      await queryRunner.manager.save(updatedUser); // this.userRepository.save(updatedUser)
+
+      // commit 성공 확정!!
+      await queryRunner.commitTransaction();
+
+      // 4. 최종결과 프론트엔드에 돌려주기
+      return pointTransaction;
+    } catch (error) {
+      // rollback 되돌리기 !!
+      await queryRunner.rollbackTransaction();
+    } finally {
+      // 연결 해재 성공하든 실패하든 실행됨 === finally
+      await queryRunner.release();
+    }
+  }
+}
